84e869d43c2c050468bbc887649c6469a800ec71
Psychodes254 <justusmasika80@gmail.com>
2025-12-01 23:38:58 +0300

    Lecture one exercise on intro to progrmamming with Python

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# Assume 3 variables are already defined for you: a, b, and c. Create a variable called total that adds a and
+# b then multiplies the result by c. Include a last line in your code to print the value: print(total)
+
+a = float(input("Type the number (a): "))
+b = float(input("Type the number (b): "))
+c = float(input("Type the number (c): "))
+
+print(f"Total = {(a + b) * c}")
