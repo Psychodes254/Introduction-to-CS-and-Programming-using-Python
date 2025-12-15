1e4c906b2ba2e645c57c001cc3b23a3be1ab3f8e
Psychodes254 <justusmasika80@gmail.com>
2025-12-16 00:04:36 +0300

    Day 10 exercise on working on list of functions

---
+++ Day 10/exercise10.py
@@ -0,0 +1,26 @@
+def all_true(n, Lf):
+    """ n is an int
+        Lf is a list of functions that take in an int and return a Boolean
+    Returns True if each and every function in Lf returns True when called 
+    with n as a parameter. Otherwise returns False. 
+    """
+    
+    for i in range(len(Lf)):
+        if not Lf[i](n):
+            return False
+        
+    return True
+     
+def is_even(n):
+    return n % 2 == 0
+
+def greater_10(n):
+    return n > 10 
+
+def less_50(n):
+    return n < 50
+
+funcs = [is_even, greater_10, less_50]
+
+   
+print(all_true(12, funcs))
