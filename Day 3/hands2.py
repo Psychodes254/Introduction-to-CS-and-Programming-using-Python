7de4d9e317bf79b881d9efa0e43c1071cb9aef65
Psychodes254 <justusmasika80@gmail.com>
2026-01-26 00:39:32 +0300

    Added solutions to hands on exercise 2 of Day 3

--- Day 3/hands2.py
+++ Day 3/hands2.py
@@ -1,10 +1,13 @@
 # What do these print?
 
 for i in range(1,4,1):
-    print(i)
+    print(i)  # 1, 2, 3
     
 for j in range(1,4,2):
-    print(j*2)
+    print(j*2)  # 2, 6
     
 for me in range(4,0,-1):
-    print("$"*me)
+    print("$"*me) #  $$$$
+                  #  $$$
+                  #  $$
+                  #  $
