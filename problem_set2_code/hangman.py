c83dc494b3655d961aa200e7a14badd157dae19f
Psychodes254 <justusmasika80@gmail.com>
2026-01-31 22:46:05 +0300

    Cleaned version of hangman

--- problem_set2_code/hangman.py
+++ problem_set2_code/hangman.py
@@ -7,9 +7,6 @@ WORDLIST_FILENAME = "words.txt"
 def load_words():
     """
     returns: list, a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
     """
     print("Loading word list from file...")
     # inFile: file
@@ -26,7 +23,6 @@ def load_words():
 def choose_word(wordlist):
     """
     wordlist (list): list of words (strings)
-
     returns: a word from wordlist at random
     """
     return random.choice(wordlist)
@@ -37,10 +33,6 @@ wordlist = load_words()
 
 def has_player_won(secret_word, letters_guessed):
     """
-    secret_word: string, the lowercase word the user is guessing
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
-
     returns: boolean, True if all the letters of secret_word are in letters_guessed,
         False otherwise
     """
@@ -54,12 +46,8 @@ def has_player_won(secret_word, letters_guessed):
 
 def get_word_progress(secret_word, letters_guessed):
     """
-    secret_word: string, the lowercase word the user is guessing
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
-
     returns: string, comprised of letters and asterisks (*) that represents
-        which letters in secret_word have not been guessed so far
+    which letters in secret_word have not been guessed so far
     """
     result_list = []
     char = list(secret_word)
@@ -77,9 +65,6 @@ def get_word_progress(secret_word, letters_guessed):
 
 def get_available_letters(letters_guessed):
     """
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
-
     returns: string, comprised of letters that represents which
       letters have not yet been guessed. The letters should be returned in
       alphabetical order
@@ -103,12 +88,11 @@ def hangman(secret_word, with_help):
 
     * The user should start with 10 guesses.
 
-    * Before each round, you should display to the user how many guesses
+    * Before each round, display to the user how many guesses
       they have left and the letters that the user has not yet guessed.
 
-    * Ask the user to supply one guess per round. Remember to make
-      sure that the user puts in a single letter (or help character '!'
-      for with_help functionality)
+    * User to supply one guess per round. The user puts in a single letter 
+      (or help character '!' for with_help functionality)
 
     * If the user inputs an incorrect consonant, then the user loses ONE guess,
       while if the user inputs an incorrect vowel (a, e, i, o, u),
@@ -117,18 +101,15 @@ def hangman(secret_word, with_help):
     * The user should receive feedback immediately after each guess
       about whether their guess appears in the computer's word.
 
-    * After each guess, you should display to the user the
-      partially guessed word so far.
+    * After each guess, display the partially guessed word so far.
 
     -----------------------------------
     with_help functionality
     -----------------------------------
-    * If the guess is the symbol !, you should reveal to the user one of the
+    * If the guess is the symbol !, reveal to the user one of the
       letters missing from the word at the cost of 3 guesses. If the user does
-      not have 3 guesses remaining, print a warning message. Otherwise, add
+      not have 3 guesses remaining, display a warning message. Otherwise, add
       this letter to their guessed word and continue playing normally.
-
-    Follows the other limitations detailed in the problem write-up.
     """
     
     print("Welcome to Hangman!")
